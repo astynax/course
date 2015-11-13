05c4703b4eea4fdea5c4388a514d0e2b0f197ffc
Aleksey <astynax84@gmail.com>
2015-11-13 17:58:18 +0300

    add a half of ``Course.Compose``

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -14,22 +14,17 @@ newtype Compose f g a =
   Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) f (Compose c) = Compose $ (f <$>) <$> c
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure = Compose . pure . pure
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
+-- Implement the (<*>) function for an Applicative instance for Compose
+  (<*>) (Compose cf) (Compose ca) = Compose $ (<*>) <$> cf <*> ca
+
+instance (Monad f, Monad g) => Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+  (=<<) f (Compose ca) = undefined -- Compose $ (_ <$>) $ (f <$>) =<< ca
+    -- error "todo: Course.Compose (<<=)#instance (Compose f g)"
